d652b1539f69744f1541ec4dc21f7bea406633f6
Mark <mark.omuga@student.moringaschool.com>
2023-10-24 17:08:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+ function distanceFromHqInBlocks(someValue) {
+    return someValue - 42;
+  }
+
+  function distanceFromHqInFeet(someValue) {
+  	return (someValue -42) * 256;
+}
+
+ function distanceTravelledInFeet(start, destination) {
+    return destination - start;
+  }
+
+  function calculatesFarePrice(start, destination) {
+   let d = destination -start;
+   if(d>=2500)
+   {
+   		return `cannot travel that far`;
+   }
+   else if(d>=2000 && d<=2500)
+   {
+   	 return   `${25} dollars`;
+   }
+   else if (d >= 400) {
+
+   	let c = 2000 - 400;
+   	let t = c / 2;
+
+   	return `${t} dollars`;
+   }
+
+   let fare = 2;
+  }
